725f352bca0e88655ec3ff74b01b5ab72b595717
Louis <louisbousquet13@gmail.com>
2024-05-23 14:09:21 +0200

    jeudi

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,48 +21,22 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   // FONCTION DISPLAY MENU MOBILE 
 
-function displayMenu() {
-    const body = document.querySelector("body");
-    const hamburger = document.querySelector(".hamburger-btn");
-    const menu = document.querySelector(".mobile-nav");
+// function displayMenu() {
+//     const body = document.querySelector("body");
+//     const hamburger = document.querySelector(".hamburger");
+//     const menu = document.querySelector(".mobile-nav");
   
-    hamburger.classList.toggle("open");
-    menu.classList.toggle("open");
-    body.classList.toggle("overflow-hidden");
-    }
+//     hamburger.classList.toggle("open");
+//     menu.classList.toggle("open");
+//     body.classList.toggle("overflow-hidden");
+//     }
 
+    document.querySelector('.hamburger').addEventListener('click', (e) => {
+        const menu = document.querySelector(".mobile-nav");
+        const body = document.querySelector("body");
+	    e.currentTarget.classList.toggle('is-active');
+        menu.classList.toggle("open");
+        body.classList.toggle("overflow-hidden");
+    })
 
-// Sélection de toutes les images à l'intérieur de la classe image-modal-content
-const lightboxImages = document.querySelectorAll('.image-modal-content img');
-
-const modalPopup = document.querySelector('.image-modal-popup');
-// Sélection dynamique de tous les éléments à l'intérieur de la popup modale
-const modalElement = element => document.querySelector(`.image-modal-popup ${element}`);
-
-
-// Sélection du body
-const body = document.querySelector('body');
-
-// Fonction pour ouvrir la modale
-function openModal(imgSrc) {
-    body.style.overflow = 'hidden'; // Empêche le défilement de la page
-    modalPopup.style.display = 'block'; // Affiche la modale
-    modalPopup.style.overflow ='auto';
-    modalElement('img').src = imgSrc; // Affiche l'image cliquée dans la modale
-}
-
-// Fermeture de la modale lors du clic sur n'importe quelle partie de la page et restauration du défilement
-document.addEventListener('click', () => {
-    body.style.overflowX = 'hidden'; // Réactive le défilement horizontal de la page
-    body.style.overflowY = 'auto'; // Désactive le défilement vertical de la page
-    modalPopup.style.display = 'none'; // Cache la modale
-});
-
-// Boucle sur chaque image pour ajouter la fonctionnalité de clic
-lightboxImages.forEach(img => {
-    img.addEventListener('click', e => {
-        e.stopPropagation(); // Empêche la propagation du clic jusqu'au document
-        openModal(img.src); // Ouvre la modale avec l'image cliquée
-    });
-});
 
